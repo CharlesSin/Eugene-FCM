630f5f4fb0350428930cbd315d076765d3723e64
Charles Sin <charlessin1993@gmail.com>
2023-01-07 15:17:41 +0800

    1. remove data cors.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const app = express();
 
 require("dotenv").config();
 
-const allowlist = ["http://127.0.0.1:3000", "http://localhost:3000", "https://charles-fcm-web.netlify.app"];
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// const allowlist = ["http://127.0.0.1:3000", "http://localhost:3000", "https://charles-fcm-web.netlify.app"];
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
 app.use(express.json({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
