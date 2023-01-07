112951385addd6db3979d356254eab3d29f74ef9
Charles Sin <charlessin1993@gmail.com>
2023-01-07 17:04:28 +0800

    1. update static init firebase function to settimeout.

--- public/javascript/init-firebase.js
+++ public/javascript/init-firebase.js
@@ -4,86 +4,86 @@ function fetchGetMethod(url) {
     .then((data) => data)
     .catch((err) => console.error(err));
 }
-console.log(fetchGetMethod("https://eugene-fcm.vercel.app/firebaseConfigKey"));
-console.log(fetchGetMethod("https://eugene-fcm.vercel.app/firebasevapidkey"));
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAen2FnXy-gKlxgeHZSgTpr-dAUsD9X7bM",
-  projectId: "eugene-fcm",
-  messagingSenderId: "909731893166",
-  appId: "1:909731893166:web:b18c5a0cdc2fcff00c823f",
-};
+setTimeout(async () => {
+  console.log(await fetchGetMethod("https://eugene-fcm.vercel.app/firebaseConfigKey"));
+  console.log(await fetchGetMethod("https://eugene-fcm.vercel.app/firebasevapidkey"));
 
-async function getVisitorData() {
-  const fp = await FingerprintJS.load({ debug: false });
-  return await fp.get();
-}
+  const firebaseConfig = {
+    apiKey: "AIzaSyAen2FnXy-gKlxgeHZSgTpr-dAUsD9X7bM",
+    projectId: "eugene-fcm",
+    messagingSenderId: "909731893166",
+    appId: "1:909731893166:web:b18c5a0cdc2fcff00c823f",
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  async function getVisitorData() {
+    const fp = await FingerprintJS.load({ debug: false });
+    return await fp.get();
+  }
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = firebase.firestore();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase Cloud Messaging and get a reference to the service
-const messaging = firebase.messaging();
+  // Initialize Firebase Cloud Messaging and get a reference to the service
+  const messaging = firebase.messaging();
 
-async function getNotificationToken() {
-  const { visitorId } = await getVisitorData();
-  document.querySelector("#vistor").textContent = visitorId;
+  async function getNotificationToken() {
+    const { visitorId } = await getVisitorData();
+    document.querySelector("#vistor").textContent = visitorId;
 
-  /**
-   * Get registration token. Initially this makes a network call, once retrieved
-   * subsequent calls to getToken will return from cache.
-   */
-  messaging
-    .getToken({ vapidKey: "BI4vxOcRLneYRWuOhoaXWLTdYmY4xChF_XudMLNyW1wreBM9kE3bdEA66AAiQPRuTvo_otmq37UedZwiiOXwoBA" })
-    .then((currentToken) => {
-      if (currentToken) {
-        document.querySelector("#token").textContent = currentToken;
-        db.collection("online-users")
-          .add({
-            token: `${currentToken}`,
-            visitor: `${visitorId}`,
-            created: `${new Date().getTime()}`,
+    /**
+     * Get registration token. Initially this makes a network call, once retrieved
+     * subsequent calls to getToken will return from cache.
+     */
+    messaging
+      .getToken({ vapidKey: "BI4vxOcRLneYRWuOhoaXWLTdYmY4xChF_XudMLNyW1wreBM9kE3bdEA66AAiQPRuTvo_otmq37UedZwiiOXwoBA" })
+      .then((currentToken) => {
+        if (currentToken) {
+          document.querySelector("#token").textContent = currentToken;
+          firebase.firestore().collection("online-users")
+            .add({
+              token: `${currentToken}`,
+              visitor: `${visitorId}`,
+              created: `${new Date().getTime()}`,
+            })
+            .catch((error) => {
+              console.error("Error adding document: ", error);
+            });
+        } else {
+          console.error("No registration token available. Request permission to generate one.");
+        }
+      })
+      .catch((err) => {
+        console.error("An error occurred while retrieving token. ", err);
+      });
+  }
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./firebase-messaging-sw.js")
+      .then(function (registration) {
+        messaging.useServiceWorker(registration);
+        messaging
+          .requestPermission()
+          .then(function () {
+            // console.log("Notification permission granted.");
+            getNotificationToken();
           })
-          .catch((error) => {
-            console.error("Error adding document: ", error);
+          .catch(function (err) {
+            console.error("Unable to get permission to notify.", err);
           });
-      } else {
-        console.error("No registration token available. Request permission to generate one.");
-      }
-    })
-    .catch((err) => {
-      console.error("An error occurred while retrieving token. ", err);
-    });
-}
+      })
+      .catch(function (err) {
+        console.error("Service worker registration failed, error:", err);
+      });
+  }
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./firebase-messaging-sw.js")
-    .then(function (registration) {
-      messaging.useServiceWorker(registration);
-      messaging
-        .requestPermission()
-        .then(function () {
-          // console.log("Notification permission granted.");
-          getNotificationToken();
-        })
-        .catch(function (err) {
-          console.error("Unable to get permission to notify.", err);
-        });
-    })
-    .catch(function (err) {
-      console.error("Service worker registration failed, error:", err);
-    });
-}
+  messaging.onTokenRefresh(function () {
+    console.log("onTokenRefresh");
+    getNotificationToken();
+  });
 
-messaging.onTokenRefresh(function () {
-  // console.log("onTokenRefresh");
-  getNotificationToken();
-});
-
-messaging.onMessage((payload) => {
-  // console.log("Message received. ", payload);
-});
+  messaging.onMessage((payload) => {
+    console.log("Message received. ", payload);
+  });
+}, 500);
