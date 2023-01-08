e314e19ad0c3fcd0f711ac0e5f7ca7fccf5e92e2
Charles Sin <charlessin1993@gmail.com>
2023-01-08 14:59:02 +0800

    1. add fcm cloud messaging server key. 2. pushnotification api change to sendbytoken api. 3. add new fetchPostData method for fcm rest api send req. 4. update env local variable file.

--- index.js
+++ index.js
@@ -31,10 +31,37 @@ app.get("/firebasevapidkey", (req, res) => {
   res.status(200).json({ config: json });
 });
 
-app.post("/pushNotification", (req, res) => {
-  const { data } = req.body;
+// Example POST method implementation:
+async function fetchPostData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: process.env.FCM_SERVER_KEY,
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+app.post("/sendbytoken", (req, res) => {
+  const { fcmToken, msgTitle, msgBody, openUri, imageUri } = req.body;
   const msg = "HELLO WORLD";
-  res.status(200).json({ msg, data });
+  const fcmMsgObject = {
+    to: fcmToken,
+    data: { msgTitle: msgTitle, msgBody: msgBody, openUri: openUri, imageUri: imageUri },
+  };
+
+  postData("https://fcm.googleapis.com/fcm/send", fcmMsgObject).then((responseData) => {
+    console.log(responseData); // JSON data parsed by `data.json()` call
+    res.status(200).json({ msg, fcmMsgObject, responseData });
+  });
 });
 
 const PORT = process.env.PORT || 8282;
