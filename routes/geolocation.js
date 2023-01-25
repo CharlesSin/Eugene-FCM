eaae0c28a978429f93bdb944c997c6b4171ce57b
Charles Sin <charlessin1993@gmail.com>
2023-01-25 09:57:07 +0800

    1. remove ip address variable.

--- routes/geolocation.js
+++ routes/geolocation.js
@@ -3,8 +3,7 @@ const request = require("request");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  // const IP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  const IP = "59.127.117.100";
+  const IP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
   request.get(
     `http://ip-api.com/json/${IP}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,reverse,mobile,proxy,hosting,query`,
