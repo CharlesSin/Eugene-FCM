73157c5b5a54f3ffea8ce5adcd758034889f237e
Charles Sin <charlessin1993@gmail.com>
2023-01-26 21:37:19 +0800

    1. add cors domain website.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5500", "https://2022-the-f2e.netlify.app"];
+const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5500", "https://2022-the-f2e.netlify.app","https://2022-the-f2e-2.netlify.app"];
 
 module.exports = allowedOrigins;
